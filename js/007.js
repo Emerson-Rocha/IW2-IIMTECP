8d98ac6c2ab11adf2ce5ff401f363e377628c31f
Emerson Rocha <52533818+Emerson-Rocha@users.noreply.github.com>
2024-04-24 10:12:56 -0300

    aqwasd

---
+++ js/007.js
@@ -0,0 +1,15 @@
+
+function fn() {
+
+    // função nomeada
+    let numero1 = document.querySelector("#numero01");
+
+    alert(numero1.value)
+    let numero2 = document.querySelector("#numero02");
+    let v1 = document.getElementById("x");
+    v1 = numero1 + numero2;
+    v1.innerHTML = "função Nomeada"
+}
+
+
+
