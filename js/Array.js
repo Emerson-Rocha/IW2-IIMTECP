5091a071eb60748c94170713b927f1a794ad1832
Emerson Rocha <52533818+Emerson-Rocha@users.noreply.github.com>
2024-05-22 10:25:59 -0300

    Update Array.js

--- js/Array.js
+++ js/Array.js
@@ -1,5 +1,3 @@
-
-// formas de criar array
 // tipos de criações de array: indice, associativo 
 // 1º forma
 
@@ -23,12 +21,64 @@ window.onload = function () {
     document.write("<br>")
 
     for (const key in frutas) {
-       // document.write(key +" : "+ frutas[key] +'<br>');
+        // document.write(key +" : "+ frutas[key] +'<br>');
         // interpolação de string
         document.write(`${key} :  ${frutas[key]}  <br>`);
 
     }
 
+
+    // 2 FORMA
+
+    let nomes = new Array('Ricardo', 'Carlos', 'Nicolas', 'Maria');
+    nomes.push('Luiza');
+    nomes.unshift('Vinicius', 'Moises', 'Sofia');
+
+    for (let i = 7; i < nomes.length; i++) {
+        console.log(nomes[i] + '\n');
+    }
+
+
+    let mudanca = nomes.map((texto) => texto + " IPI ");
+    console.log(mudanca.reverse());
+
+
+
+    // 3 forma
+
+    let objArray = [
+        {
+            nome: 'Carlos',
+            cidade: 'Sorocaba',
+            bairro: ' Vitoria Regis',
+            tipo: 'passa fome!!!'
+        },
+        {
+            nome: 'Maria',
+            cidade: 'Sor',
+            bairro: ' Vitoria',
+            tipo: 'passa fome!!!'
+        },
+
+    ]
+    
+    document.write(`<p>
+                     ${objArray[0].nome}, 
+                     ${objArray[0]['cidade']},
+                     ${objArray[0].bairro},
+                     ${objArray[0]["tipo"]},
+                  </p>`)
+
+
+    document.write(`<p>
+                  ${objArray[1].nome}, 
+                  ${objArray[1]['cidade']},
+                  ${objArray[1].bairro},
+                  ${objArray[1]["tipo"]},
+               </p>`)
+
 }
+
+
 
 
